e8d2f9519ab352b9b2889c960ba3cb972bc95449
zalmstra@xs4all.nl <zalmstra@xs4all.nl@28eeb352-58eb-11de-bc7c-c9df4fa8b3c6>
2010-04-11 18:28:31 +0000

    Start of implementation of T35

--- trunk/MicroController/timer.c
+++ trunk/MicroController/timer.c
@@ -1,5 +1,25 @@
 /**
  * \file timer.c
+ *
+ * \page timerpage The timers
+ *
+ * The TelescopeControl uses several timers. This page describes how the
+ * hardware timers in the micro controller are used.
+ *
+ * Two timers are used to control the frequency with which the motors are stepped.
+ * T1 is used to control the RA motor and T2 is used to control the declination motor.
+ *
+ * Besides these two timers we also need a periodic timer that triggers the checking of
+ * pressed keys (and in ramping the speed of the motors) and a timer that is used in the
+ * modbus protocol. Since we don't have two more hardware timers we use a software solution
+ * around T0
+ *
+ * \section t0 Timer 0
+ *
+ * \section t1 Timer 1
+ *
+ * \section t2 Timer 2
+ *
  */
 
 #include <avr/interrupt.h>
@@ -23,12 +43,84 @@ uint8_t T1OCChanged = 0;
 
 static uint8_t timer1Running = 0;
 
+static uint8_t mbTimerValue; ///< The OCR value for the modbus timer
 /**
- * \brief Interrupt routine for Timer 0 overflow
+ * \brief Initialize the modbus timer
+ *
+ * \param usTim1Timerout50us
+ * The timer value in units of 50 micro seconds
+ * This unit is used to work seamlessly with the modbus module that calls this function.
+ *
+ * We need to translate the parameter to the OCR value for timer 0
+ */
+BOOL
+xMBPortTimersInit( USHORT usTim1Timerout50us )
+{
+    /* Calculate overflow counter an OCR values for Timer0. */
+	/*
+	 * The required length is usTim1Timerout50us * 50 / 1000000
+	 * The time between clock pulses is  T0_PRESCALER / F_CPU
+	 */
+	mbTimerValue = (usTim1Timerout50us * F_CPU) / (TO_PRESCALER * 200000);
+
+    vMBPortTimersDisable(  );
+
+    return TRUE;
+}
+
+uint8_t nextTimerValue = 0;
+enum {
+	periodic,
+	T35
+} activeTimer;
+
+inline void
+vMBPortTimersEnable(  )
+{
+
+}
+
+inline void
+vMBPortTimersDisable(  )
+{
+	cli();
+    if (activeTimer == periodic) {
+    	nextTimerValue = 0;
+    } else {
+    	uint8_t timeLeft = OCR0 - TCNT0 + nextTimerValue;
+    	TCNT0 = 0;
+    	OCR0 = timeLeft;
+    	activeTimer = periodic;
+    }
+    sei();
+}
+/**
+ * \brief Interrupt routine for Timer 0 output compare
  */
 ISR(TIMER0_COMP_vect)
 {
-	SetEvent(tmr0);
+	if (activeTimer == periodic) {
+		SetEvent(tmr0);
+		if (nextTimerValue) {
+			OCR0 = nextTimerValue;
+			activeTimer = T35;
+			nextTimerValue = 0;
+		}
+		else {
+			OCR0 = OCR0_VAL;
+		}
+	}
+	else {
+		if (nextTimerValue) {
+			OCR0 = nextTimerValue;
+			nextTimerValue = 0;
+		}
+		else {
+			OCR0 = OCR0_VAL;
+		}
+		pxMBPortCBTimerExpired();
+		activeTimer = periodic;
+	}
 }
 
 
@@ -51,13 +143,17 @@ ISR(TIMER1_COMPA_vect)
 
 void TimerInit()
 {
-	// Setup Timer 0
+	// Setup Timer 0. This timer is used for a periodic 10msec timer
+	// which is running continuously and the t3.5 timer used in modbus
+	// RTU communication
 	TCCR0 = _BV(WGM01); // CTC mode
 	TCCR0 |= (T0_PRESCALE_SELECT << CS00); // select prescale
 
 	OCR0 = OCR0_VAL;
 	TIMSK = _BV(OCIE0);
+	activeTimer = periodic;
 
+	// Setup timer 1. This is the timer that controls the RA motor
 	OC1A_DDR |= _BV(OC1A_PIN);
 	OC1A_PORT &= ~_BV(OC1A_PIN); // Start with pin == 0
 
