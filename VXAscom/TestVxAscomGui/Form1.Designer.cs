9f9c2265981a15b3c78f08145b7ff6b369a5efea
zalmstra <zalmstra@xs4all.nl>
2010-09-27 08:21:48 +0000

    Baseline before changing to Modbus interface

--- VXAscom/TestVxAscomGui/Form1.Designer.cs
+++ VXAscom/TestVxAscomGui/Form1.Designer.cs
@@ -33,17 +33,19 @@
             System.Windows.Forms.Label lSTStringLabel;
             System.Windows.Forms.Label raMoveRateLabel;
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.btnSelect = new System.Windows.Forms.Button();
             this.btnSetup = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.tmrAutoUpdate = new System.Windows.Forms.Timer(this.components);
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.raStringTextBox = new System.Windows.Forms.TextBox();
-            this.lSTStringTextBox = new System.Windows.Forms.TextBox();
+            this.naCtrlRA = new AngleDisplay.NewAngleControl();
             this.trackingCheckBox = new System.Windows.Forms.CheckBox();
+            this.lSTStringTextBox = new System.Windows.Forms.TextBox();
+            this.raStringTextBox = new System.Windows.Forms.TextBox();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.raMoveRateTextBox = new System.Windows.Forms.TextBox();
             this.chkMoveRa = new System.Windows.Forms.CheckBox();
+            this.raMoveRateTextBox = new System.Windows.Forms.TextBox();
             this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
             raStringLabel = new System.Windows.Forms.Label();
             lSTStringLabel = new System.Windows.Forms.Label();
@@ -54,8 +56,36 @@
             ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
             this.SuspendLayout();
             // 
+            // raStringLabel
+            // 
+            raStringLabel.AutoSize = true;
+            raStringLabel.Location = new System.Drawing.Point(3, 46);
+            raStringLabel.Name = "raStringLabel";
+            raStringLabel.Size = new System.Drawing.Size(22, 13);
+            raStringLabel.TabIndex = 0;
+            raStringLabel.Text = "RA";
+            // 
+            // lSTStringLabel
+            // 
+            lSTStringLabel.AutoSize = true;
+            lSTStringLabel.Location = new System.Drawing.Point(0, 20);
+            lSTStringLabel.Name = "lSTStringLabel";
+            lSTStringLabel.Size = new System.Drawing.Size(27, 13);
+            lSTStringLabel.TabIndex = 2;
+            lSTStringLabel.Text = "LST";
+            // 
+            // raMoveRateLabel
+            // 
+            raMoveRateLabel.AutoSize = true;
+            raMoveRateLabel.Location = new System.Drawing.Point(5, 22);
+            raMoveRateLabel.Name = "raMoveRateLabel";
+            raMoveRateLabel.Size = new System.Drawing.Size(22, 13);
+            raMoveRateLabel.TabIndex = 0;
+            raMoveRateLabel.Text = "RA";
+            // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.btnSelect);
             this.groupBox1.Controls.Add(this.btnSetup);
             this.groupBox1.Controls.Add(this.nameTextBox);
             this.groupBox1.Controls.Add(this.label1);
@@ -66,24 +96,15 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Driver";
             // 
-            // label1
+            // btnSelect
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(18, 23);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Telescope";
-            // 
-            // nameTextBox
-            // 
-            this.nameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.form1BindingSource, "Driver.Name", true));
-            this.nameTextBox.Location = new System.Drawing.Point(81, 19);
-            this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.ReadOnly = true;
-            this.nameTextBox.Size = new System.Drawing.Size(196, 20);
-            this.nameTextBox.TabIndex = 3;
-            this.nameTextBox.TabStop = false;
+            this.btnSelect.Location = new System.Drawing.Point(111, 54);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(75, 23);
+            this.btnSelect.TabIndex = 5;
+            this.btnSelect.Text = "Select";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
             // 
             // btnSetup
             // 
@@ -95,15 +116,33 @@
             this.btnSetup.UseVisualStyleBackColor = true;
             this.btnSetup.Click += new System.EventHandler(this.btnSetup_Click);
             // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(81, 19);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.ReadOnly = true;
+            this.nameTextBox.Size = new System.Drawing.Size(196, 20);
+            this.nameTextBox.TabIndex = 3;
+            this.nameTextBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Telescope";
+            // 
             // tmrAutoUpdate
             // 
-            this.tmrAutoUpdate.Interval = 500;
+            this.tmrAutoUpdate.Interval = 1000;
             this.tmrAutoUpdate.Tick += new System.EventHandler(this.tmrAutoUpdate_Tick);
             // 
             // groupBox2
             // 
-            this.groupBox2.AutoSize = true;
             this.groupBox2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox2.Controls.Add(this.naCtrlRA);
             this.groupBox2.Controls.Add(this.trackingCheckBox);
             this.groupBox2.Controls.Add(lSTStringLabel);
             this.groupBox2.Controls.Add(this.lSTStringTextBox);
@@ -111,19 +150,43 @@
             this.groupBox2.Controls.Add(this.raStringTextBox);
             this.groupBox2.Location = new System.Drawing.Point(3, 108);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(224, 82);
+            this.groupBox2.Size = new System.Drawing.Size(220, 111);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Position";
             // 
-            // raStringLabel
+            // naCtrlRA
             // 
-            raStringLabel.AutoSize = true;
-            raStringLabel.Location = new System.Drawing.Point(3, 46);
-            raStringLabel.Name = "raStringLabel";
-            raStringLabel.Size = new System.Drawing.Size(22, 13);
-            raStringLabel.TabIndex = 0;
-            raStringLabel.Text = "RA";
+            this.naCtrlRA.AutoSize = true;
+            this.naCtrlRA.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.naCtrlRA.DataBindings.Add(new System.Windows.Forms.Binding("Angle", this.form1BindingSource, "RA", true));
+            this.naCtrlRA.Label = "RA";
+            this.naCtrlRA.Location = new System.Drawing.Point(6, 78);
+            this.naCtrlRA.Margin = new System.Windows.Forms.Padding(0);
+            this.naCtrlRA.Name = "naCtrlRA";
+            this.naCtrlRA.Size = new System.Drawing.Size(123, 20);
+            this.naCtrlRA.TabIndex = 6;
+            // 
+            // trackingCheckBox
+            // 
+            this.trackingCheckBox.AutoSize = true;
+            this.trackingCheckBox.DataBindings.Add(new System.Windows.Forms.Binding("Checked", this.form1BindingSource, "Tracking", true));
+            this.trackingCheckBox.Location = new System.Drawing.Point(150, 45);
+            this.trackingCheckBox.Name = "trackingCheckBox";
+            this.trackingCheckBox.Size = new System.Drawing.Size(68, 17);
+            this.trackingCheckBox.TabIndex = 5;
+            this.trackingCheckBox.Text = "Tracking";
+            this.trackingCheckBox.UseVisualStyleBackColor = true;
+            this.trackingCheckBox.CheckedChanged += new System.EventHandler(this.trackingCheckBox_CheckedChanged);
+            // 
+            // lSTStringTextBox
+            // 
+            this.lSTStringTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.form1BindingSource, "LstString", true));
+            this.lSTStringTextBox.Location = new System.Drawing.Point(31, 17);
+            this.lSTStringTextBox.Name = "lSTStringTextBox";
+            this.lSTStringTextBox.ReadOnly = true;
+            this.lSTStringTextBox.Size = new System.Drawing.Size(100, 20);
+            this.lSTStringTextBox.TabIndex = 3;
             // 
             // raStringTextBox
             // 
@@ -134,64 +197,17 @@
             this.raStringTextBox.Size = new System.Drawing.Size(100, 20);
             this.raStringTextBox.TabIndex = 1;
             // 
-            // lSTStringLabel
-            // 
-            lSTStringLabel.AutoSize = true;
-            lSTStringLabel.Location = new System.Drawing.Point(0, 20);
-            lSTStringLabel.Name = "lSTStringLabel";
-            lSTStringLabel.Size = new System.Drawing.Size(27, 13);
-            lSTStringLabel.TabIndex = 2;
-            lSTStringLabel.Text = "LST";
-            // 
-            // lSTStringTextBox
-            // 
-            this.lSTStringTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.form1BindingSource, "LSTString", true));
-            this.lSTStringTextBox.Location = new System.Drawing.Point(31, 17);
-            this.lSTStringTextBox.Name = "lSTStringTextBox";
-            this.lSTStringTextBox.ReadOnly = true;
-            this.lSTStringTextBox.Size = new System.Drawing.Size(100, 20);
-            this.lSTStringTextBox.TabIndex = 3;
-            // 
-            // trackingCheckBox
-            // 
-            this.trackingCheckBox.AutoSize = true;
-            this.trackingCheckBox.DataBindings.Add(new System.Windows.Forms.Binding("CheckState", this.form1BindingSource, "Driver.Tracking", true));
-            this.trackingCheckBox.Location = new System.Drawing.Point(150, 45);
-            this.trackingCheckBox.Name = "trackingCheckBox";
-            this.trackingCheckBox.Size = new System.Drawing.Size(68, 17);
-            this.trackingCheckBox.TabIndex = 5;
-            this.trackingCheckBox.Text = "Tracking";
-            this.trackingCheckBox.UseVisualStyleBackColor = true;
-            // 
             // groupBox3
             // 
             this.groupBox3.Controls.Add(this.chkMoveRa);
             this.groupBox3.Controls.Add(raMoveRateLabel);
             this.groupBox3.Controls.Add(this.raMoveRateTextBox);
-            this.groupBox3.Location = new System.Drawing.Point(3, 208);
+            this.groupBox3.Location = new System.Drawing.Point(3, 249);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(220, 100);
             this.groupBox3.TabIndex = 2;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "MoveAxis";
-            // 
-            // raMoveRateLabel
-            // 
-            raMoveRateLabel.AutoSize = true;
-            raMoveRateLabel.Location = new System.Drawing.Point(5, 22);
-            raMoveRateLabel.Name = "raMoveRateLabel";
-            raMoveRateLabel.Size = new System.Drawing.Size(22, 13);
-            raMoveRateLabel.TabIndex = 0;
-            raMoveRateLabel.Text = "RA";
-            // 
-            // raMoveRateTextBox
-            // 
-            this.raMoveRateTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.form1BindingSource, "RaMoveRate", true));
-            this.raMoveRateTextBox.Location = new System.Drawing.Point(33, 19);
-            this.raMoveRateTextBox.Name = "raMoveRateTextBox";
-            this.raMoveRateTextBox.Size = new System.Drawing.Size(87, 20);
-            this.raMoveRateTextBox.TabIndex = 1;
-            this.raMoveRateTextBox.Text = "15.3";
             // 
             // chkMoveRa
             // 
@@ -204,9 +220,17 @@
             this.chkMoveRa.UseVisualStyleBackColor = true;
             this.chkMoveRa.CheckedChanged += new System.EventHandler(this.chkMoveRa_CheckedChanged);
             // 
+            // raMoveRateTextBox
+            // 
+            this.raMoveRateTextBox.Location = new System.Drawing.Point(33, 19);
+            this.raMoveRateTextBox.Name = "raMoveRateTextBox";
+            this.raMoveRateTextBox.Size = new System.Drawing.Size(87, 20);
+            this.raMoveRateTextBox.TabIndex = 1;
+            this.raMoveRateTextBox.Text = "15.3";
+            // 
             // form1BindingSource
             // 
-            this.form1BindingSource.DataSource = typeof(TestVxAscomGui.Form1);
+            this.form1BindingSource.DataSource = typeof(TestVxAscomGui.DriverData);
             // 
             // Form1
             // 
@@ -228,7 +252,6 @@
             this.groupBox3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -247,6 +270,8 @@
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.TextBox raMoveRateTextBox;
         private System.Windows.Forms.CheckBox chkMoveRa;
+        private System.Windows.Forms.Button btnSelect;
+        private AngleDisplay.NewAngleControl naCtrlRA;
     }
 }
 
