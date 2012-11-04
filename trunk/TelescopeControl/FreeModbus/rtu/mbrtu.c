5e7d4e7567aa1faeeaa62e5686ccdfc66332a2a4
zalmstra@xs4all.nl <zalmstra@xs4all.nl@28eeb352-58eb-11de-bc7c-c9df4fa8b3c6>
2012-11-04 11:04:15 +0000

    Build and link successful

--- trunk/TelescopeControl/FreeModbus/rtu/mbrtu.c
+++ trunk/TelescopeControl/FreeModbus/rtu/mbrtu.c
@@ -42,7 +42,7 @@
 
 #include "mbcrc.h"
 #include "mbport.h"
-#include "timer.h"
+//#include "timer.h"
 
 /* ----------------------- Defines ------------------------------------------*/
 #define MB_SER_PDU_SIZE_MIN     4       /*!< Minimum size of a Modbus RTU frame. */
